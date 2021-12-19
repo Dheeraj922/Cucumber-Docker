ded93a34b7297bba6182047674985b32dcce020f
Dheeraj Choudhary <dheeraj.choudhary@sprinklr.com>
2021-12-19 22:37:31 +0530

    refactor code

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,40 +2,24 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
-
-import java.net.MalformedURLException;
-import java.net.UnknownHostException;
-
 
 @CucumberOptions(features = "src/test/resources/features",
                 glue = {"steps","hooks"},
                 plugin = {"pretty","json:target/cucumber-reports/cucumber.json", "html:target/cucumber-html-reports"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-    WebDriver driver;
-
     @BeforeSuite
-    public void beforeSuite() throws UnknownHostException, MalformedURLException {
+    public void beforeSuite() {
         System.out.println("From TestNG Before Suite");
-//        // LOCAL Execution
-//        driver = initDriver(Browser.CHROME);
-//
-//        // GRID Execution
-//        // Take input for browser Name from Config
-////        driver = initDriver(Browser.CHROME, ExecutionType.GRID);
-
     }
 
     @AfterSuite
     public void afterSuite() {
         System.out.println("From TestNG After Suite");
-//        driver.quit();
     }
-
 
     @Override
     @DataProvider(parallel = true)
